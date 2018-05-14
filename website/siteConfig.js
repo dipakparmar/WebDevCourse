5f3ecf6dc27b7c7c17c7bb623bce686fb7927948
Dipak Parmar <24366206+dipakparmar@users.noreply.github.com>
2018-05-13 19:29:16 -0700

    Update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'WebSite Development Course' /* title for your website */,
   tagline: 'Build a Website using Wordpress',
   url: 'https://dipakparmar.github.io/',
-  baseUrl: '/webdevcourse/' /* base url for your project */,
+  baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
